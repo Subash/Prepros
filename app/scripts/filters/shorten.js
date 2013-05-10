28a7e46e1f55394800547432dc0c0c4e1ec6a1d8
Subash Pathak <sbshpthk@gmail.com>
2013-05-10 08:38:12 +0545

    Shorten file output and input path Fix #3

---
+++ app/scripts/filters/shorten.js
@@ -0,0 +1,34 @@
+/*jshint browser: true, node: true*/
+/*global prepros, _ */
+
+'use strict';
+
+//Filter to shorten the string
+prepros.filter('shorten', function () {
+    return function (string) {
+        if(string.length>65){
+
+            var newLength = string.length - 75;
+
+            var splits = string.split('/');
+
+            var first = splits[0];
+
+            //Doesn't work in edge cases; please submit a pull request if you have better idea
+            var i =1;
+            while(first.length < 25){
+
+                first = first + '/' + splits[i];
+                i++;
+            }
+
+            //File name and folder where it resides
+            var last = splits[splits.length - 2] + '/' +splits[splits.length - 1];
+
+            return first + '/........../' + last;
+
+        } else {
+            return string;
+        }
+    };
+});
