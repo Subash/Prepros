426cfbb1acb0eb85752382ffb3d51c72bba5c028
Subash Pathak <sbspk@msn.com>
2013-06-13 12:30:03 +0545

    javascript prepend

--- app/scripts/services/fileTypes/javascript.js
+++ app/scripts/services/fileTypes/javascript.js
@@ -8,7 +8,7 @@
 /*jshint browser: true, node: true, loopfunc: true*/
 /*global prepros, _*/
 
-prepros.factory('javascript', function (config, utils, importsVisitor) {
+prepros.factory('javascript', function (config, utils) {
 
     'use strict';
 
@@ -64,7 +64,8 @@ prepros.factory('javascript', function (config, utils, importsVisitor) {
 
             } else {
 
-                var javascript = data.toString();
+                var javascript = data.toString(),
+                    error;
 
                 if (file.config.uglify) {
 
@@ -80,101 +81,135 @@ prepros.factory('javascript', function (config, utils, importsVisitor) {
                     }
                 }
 
-                var result, importedFile, error = false, fileImports = [];
-
-                var importReg = /\/\/(?:\s|)@prepros-append\s+(.*)/gi;
+                var importReg = {
+                    append: /\/\/(?:\s|)@prepros-append\s+(.*)/gi,
+                    prepend: /\/\/(?:\s|)@prepros-prepend\s+(.*)/gi
+                };
 
                 var read = function(filePathToRead) {
 
-                    var impFiles = [];
+                    var importedFiles = {
+                        append : [],
+                        prepend : []
+                    };
 
-                    while ((result = importReg.exec(fs.readFileSync(filePathToRead))) !== null) {
+                    var regs = Object.keys(importReg);
 
-                        result[1] = result[1].replace(/'|"/gi, '').trim();
+                    _.each(regs, function(reg) {
 
-                        //Check if path is full or just relative
-                        if (result[1].indexOf(':') >= 0) {
+                        var result;
 
-                            importedFile = path.normalize(result[1]);
+                        while ((result = importReg[reg].exec(fs.readFileSync(filePathToRead))) !== null) {
 
-                        } else {
+                            var importedFile;
 
-                            importedFile = path.join(path.dirname(filePathToRead), result[1]);
-                        }
+                            result[1] = result[1].replace(/'|"/gi, '').trim();
 
-                        //Add extension
-                        if (path.extname(importedFile).toLowerCase() !== '.js') {
+                            //Check if path is full or just relative
+                            if (result[1].indexOf(':') >= 0) {
 
-                            importedFile = importedFile + '.js';
-                        }
+                                importedFile = path.normalize(result[1]);
 
-                        //Check if file exists
-                        if (fs.existsSync(importedFile)) {
+                            } else {
 
-                            impFiles.push(importedFile);
+                                importedFile = path.join(path.dirname(filePathToRead), result[1]);
+                            }
 
-                        } else {
+                            //Check if file exists
+                            if (fs.existsSync(importedFile)) {
 
-                            error = true;
+                                importedFiles[reg].push(importedFile);
 
-                            callback(true, 'Imported file "'+ importedFile +'" not found \n' + file.input);
-                        }
-                    }
-
-                    return impFiles;
-                };
-
-                fileImports[0] = read(file.input);
-
-                //Get imports of imports up to four levels
-                for(var i=1; i<5; i++) {
-
-                    fileImports[i] = [];
-
-                    _.each(fileImports[i-1], function(importedFile){
-
-                        fileImports[i] = _.uniq(_.union(fileImports[i], read(importedFile)));
-
-                    });
-                }
-
-                try {
-
-                    //Remove repeated imports
-                    _.each(_.uniq(_.flatten(fileImports)), function(imp) {
-
-                        var js = fs.readFileSync(imp).toString();
-
-                        if (file.config.uglify && !/min.js$/.exec(path.basename(imp))) {
-
-                            try {
-
-                                js = ugly.minify(js, {fromString: true}).code;
-
-                            } catch(e) {
+                            } else {
 
                                 error = true;
 
-                                callback(true, 'Error on line '+ e.line + ' col ' + e.col + ' '+ e.message + ' of '+ imp);
+                                callback(true, 'Imported file "'+ importedFile +'" not found \n' + file.input);
                             }
                         }
-
-                        javascript = javascript + '\n'  + js;
                     });
 
-                } catch(e) {
+                    return {
+                        append: importedFiles.append,
+                        prepend: importedFiles.prepend.reverse()
+                    };
+                };
 
-                    error = true;
+                var get = function(append){
 
-                    callback(true, e.message);
+                    var imps =[];
+                    imps[0] = (append)? read(file.input).append: read(file.input).prepend;
 
-                }
+                    //Get imports of imports up to four levels
+                    for(var i=1; i<5; i++) {
 
+                        imps[i] = [];
+
+                        _.each(imps[i-1], function(importedFile){
+
+                            imps[i] = _.uniq(_.union(imps[i], (append)? read(importedFile).append: read(importedFile).prepend));
+                        });
+                    }
+
+                    return _.uniq(_.flatten(imps));
+
+                };
+
+                var join = function(files, append) {
+
+                    try {
+
+                        //Remove repeated imports
+                        _.each(_.uniq(_.flatten(files)), function(imp) {
+
+                            var js = fs.readFileSync(imp).toString();
+
+                            if (file.config.uglify && !/min.js$/.exec(path.basename(imp))) {
+
+                                try {
+
+                                    js = ugly.minify(js, {fromString: true}).code;
+
+                                } catch(e) {
+
+                                    error = true;
+
+                                    callback(true, 'Error on line '+ e.line + ' col ' + e.col + ' '+ e.message + ' of '+ imp);
+                                }
+                            }
+
+                            if(append) {
+
+                                javascript = javascript + '\n'  + js;
+
+                            } else {
+
+                                javascript = js + '\n' + javascript;
+                            }
+
+                        });
+
+                    } catch(e) {
+
+                        error = true;
+
+                        callback(true, e.message);
+                    }
+                };
+
+                //Join Files
+                var appends = get(true);
+                var prepends = get(false);
+                join(appends, true);
+                join(prepends, false);
 
                 if(!error) {
 
-                    //Remove @prepros-append statements
-                    javascript = javascript.replace(importReg, '');
+                    _.each(importReg, function(reg){
+
+                        javascript = javascript.replace(new RegExp(reg.source + '\n', 'gi'), '');
+
+                    });
 
                     fs.outputFile(file.output, javascript, function (err) {
 
