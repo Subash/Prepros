239a5d08191738f6d2209a0367f9566e91e6dd15
Matt Clements <matt@mattclements.co.uk>
2013-07-29 22:58:06 +0100

    Rolled back node-webkit version to fix OSX Notification Bug

--- build/osx/build.sh
+++ build/osx/build.sh
@@ -63,8 +63,8 @@ cd ~/.prepros_build
 
 
 #Download Node-Webkit for OSX
-wget https://s3.amazonaws.com/node-webkit/v0.6.2/node-webkit-v0.6.2-osx-ia32.zip
-unzip node-webkit-v0.6.2-osx-ia32.zip
+wget https://s3.amazonaws.com/node-webkit/v0.6.1/node-webkit-v0.6.1-osx-ia32.zip
+unzip node-webkit-v0.6.1-osx-ia32.zip
 cd node-webkit.app/Contents/Resources
 
 #Cleanup Un-needed files
