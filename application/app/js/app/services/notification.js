cb14942b2b9f99fec9f1048a716477b08ed825ee
Subash Pathak <subash@subash.me>
2013-08-14 20:54:08 +0545

    increased top offset

--- application/app/js/app/services/notification.js
+++ application/app/js/app/services/notification.js
@@ -39,7 +39,7 @@ prepros.factory('notification', function (config, $location, $rootScope) {
             };
 
             if (process.platform !== 'win32') {
-                options.y = 10;
+                options.y = 30;
             }
 
             notificationWindow = require('nw.gui').Window.open(notificationPath, options);
