e508040af749fff4bccdfb6a5d51266276c9a404
Subash Pathak <subash@subash.me>
2013-08-15 18:13:46 +0545

    ux improvements

--- application/app/js/app/services/exceptionHandler.js
+++ application/app/js/app/services/exceptionHandler.js
@@ -20,16 +20,30 @@ prepros.factory('$exceptionHandler',function(){
 
     //Save exception data to file
     process.on('uncaughtException', function(err) {
+
         var errorLogPath = require('path').join(require('nw.gui').App.dataPath[0], 'prepros-error-log.txt');
         require('fs-extra').appendFile(errorLogPath, '\n[ ' + new Date().toDateString() + ' ]\n' + err.stack.toString() + '\n');
-        window.alert('An exception occurred.\n Log File : ' + errorLogPath);
+
+        //Disable actions to prevent further errors
+        $('body').css({pointerEvents: 'none'});
+        $('.wrapper').css({opacity: '0.5'});
+        $('#title-bar-close-button').css({pointerEvents: 'auto'});
+
         console.error(err.stack);
+        window.alert('An exception occurred.\n ' + errorLogPath);
     });
 
     return function(err){
+
         var errorLogPath = require('path').join(require('nw.gui').App.dataPath[0], 'prepros-error-log.txt');
         require('fs-extra').appendFile(errorLogPath, '\n[ ' + new Date().toDateString() + ' ]\n' + err.stack.toString() + '\n');
-        window.alert('An exception occurred.\n Log File : ' + errorLogPath);
+
+        //Disable actions to prevent further errors
+        $('body').css({pointerEvents: 'none'});
+        $('.wrapper').css({opacity: '0.5'});
+        $('#title-bar-close-button').css({pointerEvents: 'auto'});
+
         console.error(err.stack);
+        window.alert('An exception occurred.\n ' + errorLogPath);
     };
 });
