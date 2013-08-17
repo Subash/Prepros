31ffcd92da976434677ad0e8848e5d4ff0fc447f
Subash Pathak <subash@subash.me>
2013-08-17 09:10:06 +0545

    fixed build

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./node_modules/jshint/bin/hint ./application/app/js/app/*
+./node_modules/jshint/bin/hint ./application/app/scripts*
 hint=$?
 if [ $hint != 0 ]; then
 	echo "< script runner stopped jshint failed >";
