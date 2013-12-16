92035a0efd0a12cd845ddf7d670316727ac2cf18
Subash <subash@subash.me>
2013-12-16 11:57:04 +0545

    removed ci

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-./node_modules/jshint/bin/hint ./application/app/scripts*
-hint=$?
-if [ $hint != 0 ]; then
-	echo "< script runner stopped jshint failed >";
-	exit $hint
-else
-	echo "< jshint passed >";
-fi
